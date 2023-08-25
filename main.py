4e919d20bbb5eac3e215b507bc9a10226ff99368
Madhurjya Dasgupta <madhurjya2003@icloud.com>
2023-08-25 14:22:07 +0530

    Created the model and server

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response
+from threading import Thread
+from generator import gen
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def run_app(port):
+    app.run(host='0.0.0.0', port=port)
+
+
+if __name__ == '__main__':
+    t1 = Thread(target=run_app, args=(5000,))
+    t1.start()
+    t2 = Thread(target=run_app, args=(5001,))
+    t2.start()
